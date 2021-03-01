91b2a4d97d1a5023c3f299883545af74f00e8575
med100s <61676013+med100s@users.noreply.github.com>
2021-03-02 01:12:38 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,3 +9,26 @@ soup = BeautifulSoup(page.text, 'html.parser')
 raw = soup.find('div', class_='blogpost').text
 
 print (raw)
+
+
+
+
+
+#addition to parse colors
+#
+#from cssutils import parseStyle
+#
+#
+#
+#soup = BeautifulSoup(page, features="lxml")
+##
+#
+#for i in soup.select("option"):
+#style = parseStyle(soup.option['style'])
+#	
+#    print(i.text)
+#    style = parseStyle(i['style'])
+#    print(style['background-color'])
+#
+#
+#style = parseStyle(soup.option['style'])
